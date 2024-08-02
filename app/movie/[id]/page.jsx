9657a21ebbcae3394fc7370a3688ed1925cd364f
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2024-08-02 16:35:15 +0200

    add trailer

--- app/movie/[id]/page.jsx
+++ app/movie/[id]/page.jsx
@@ -7,6 +7,7 @@ import "react-loading-skeleton/dist/skeleton.css";
 
 function MovieDetails({ params }) {
   const [movie, setMovie] = useState(null);
+  const [trailerUrl, setTrailerUrl] = useState(null);
   const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
   useEffect(() => {
@@ -17,7 +18,21 @@ function MovieDetails({ params }) {
       console.log(data);
       setMovie(data);
     };
+
+    const getTrailer = async () => {
+      const url = `https://api.themoviedb.org/3/movie/${params.id}/videos?api_key=${API_KEY}`;
+      const res = await fetch(url);
+      const data = await res.json();
+      const trailer = data.results.find(
+        (video) => video.type === "Trailer" && video.site === "YouTube"
+      );
+      if (trailer) {
+        setTrailerUrl(`https://www.youtube.com/embed/${trailer.key}`);
+      }
+    };
+
     getMovie();
+    getTrailer();
   }, [params.id, API_KEY]);
 
   const posterUrl = movie
@@ -25,35 +40,57 @@ function MovieDetails({ params }) {
     : null;
 
   return (
-    <section className="flex justify-between sm:flex-row flex-col max-w-[1000px] p-5 gap-5 ">
-      {movie ? (
-        <img
-          src={posterUrl}
-          alt="movie poster"
-          className="sm:w-[300px] sm:h-[500px] object-cover rounded-lg"
-        />
-      ) : (
-        <Skeleton width={300} height={500} />
-      )}
-      <div className="flex-1">
-        <p className="text-2xl font-bold mb-5">
-          {movie ? movie.title : <Skeleton width={200} />}
-        </p>
-        <p className="mb-4 font-medium text-xl">
-          {movie ? movie.overview : <Skeleton count={3} />}
-        </p>
-        <div className="flex gap-1 font-bold text-xl justify-start items-center mb-4">
-          <FaStar className="text-yellow-500" />
-          {movie ? movie.vote_average.toFixed(1) : <Skeleton width={50} />}
+    <section>
+      <div className="flex justify-between sm:flex-row flex-col max-w-[1000px] p-5 gap-6 ">
+        {movie ? (
+          <img
+            src={posterUrl}
+            alt="movie poster"
+            className="sm:w-[300px] sm:h-[500px] object-cover rounded-lg"
+          />
+        ) : (
+          <Skeleton width={300} height={500} />
+        )}
+        <div className="flex-1 sm:mt-5">
+          <p className="text-3xl font-bold mb-5">
+            {movie ? movie.title : <Skeleton width={200} />}
+          </p>
+          <p className="mb-4 font-medium text-xl">
+            {movie ? movie.overview : <Skeleton count={3} />}
+          </p>
+          <div className="flex gap-1 font-bold text-xl justify-start items-center mb-4">
+            <FaStar className="text-yellow-500" />
+            {movie ? movie.vote_average.toFixed(1) : <Skeleton width={50} />}
+          </div>
+          <p>
+            {movie ? (
+              `Release Date: ${movie.release_date}`
+            ) : (
+              <Skeleton width={150} />
+            )}
+          </p>
+          <div className="mt-6 text-xl font-bold">
+            {movie?.genres.map((genre) => genre.name).join(", ") || (
+              <Skeleton width={150} />
+            )}
+          </div>
         </div>
-        <p>
-          {movie ? (
-            `Release Date: ${movie.release_date}`
-          ) : (
-            <Skeleton width={150} />
-          )}
-        </p>
       </div>
+
+      {trailerUrl ? (
+        <div className=" flex justify-center items-center w-full mt-20 mb-20">
+          <iframe
+            width="90%"
+            height="500"
+            src={trailerUrl}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      ) : (
+        <Skeleton width={560} height={315} />
+      )}
     </section>
   );
 }
