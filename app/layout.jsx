82c3b85bc90bd16c38f91d56a410040fae51f5b2
Coday-aw <138451310+Coday-aw@users.noreply.github.com>
2024-08-14 12:00:48 +0200

    last

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
---
+++ components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div className=" text-gray-400 text-center mt-10 mb-10 text-xs ">
+      <strong>Disclaimer:</strong> This product uses the TMDb API but is not
+      endorsed or certified by TMDb. All data provided, including movie titles,
+      descriptions, and images, are sourced from The Movie Database (TMDb).
+    </div>
+  );
+}
+export default Footer;
